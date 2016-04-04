c83b0cfce6a5beb68efb3fc8eedcd5ee0557d2db
Nicolas Guilbert <nicolas@ange.dk>
2016-04-04 21:45:14 +0200

    Represent network graphs in a row rather than a 2D grid

--- model/dashboard.js
+++ model/dashboard.js
@@ -4,15 +4,12 @@ var dataset = [];
 var valueVsHonestyGraph = new liveGraph("#valueVsHonestyGraph", dataset);
 var trustVsHonestyGraph = new liveGraph("#trustVsHonestyGraph", dataset);
 var circularGraphs = [];
-for (i = 0; i < 2; ++i) {
-    circularGraphs[i] = [];
-    for (j = 0; j < 2; j++) {
-        var divName = "circularGraph" + i + "_" + j;
-        var div = d3.select("body").append("div");
-        div.attr("id", divName)
-           .attr("class", "smallgraph");
-        circularGraphs[i][j] = new circularGraph("#" + divName, {diameter: 200});
-    }
+for (i = 0; i < 4; ++i) {
+    var divName = "circularGraph" + i;
+    var div = d3.select("body").append("div");
+    div.attr("id", divName)
+       .attr("class", "smallgraph");
+    circularGraphs[i] = new circularGraph("#" + divName, {diameter: 200});
 }
 
 var interval = setInterval(function() {
@@ -46,16 +43,6 @@ var interval = setInterval(function() {
     }, []);
     var circularGraphData = { nodes : result,
                              links : relationList };
-    var i,j;
-    if (honestyCoeff < .25) {
-        i = 0; j = 0;
-    } else if (honestyCoeff >= .25 && honestyCoeff < .5) {
-        i = 0; j = 1;
-    } else if (honestyCoeff >= .5 && honestyCoeff < .75) {
-        i = 1; j = 0;
-    } else if (honestyCoeff >= .75 && honestyCoeff < 1) {
-        i = 1; j = 1;
-    }
-    circularGraphs[i][j].update(circularGraphData);
+    circularGraphs[Math.floor(honestyCoeff * 4)].update(circularGraphData);
 
 }, 100);
