c0f0a1548070ee7d60f59d4a3544a92ab0224ffa
Nicolas Guilbert <nicolas@ange.dk>
2016-04-04 17:28:17 +0200

    Fix graph alignment

--- model/dashboard.js
+++ model/dashboard.js
@@ -9,7 +9,8 @@ for (i = 0; i < 2; ++i) {
     for (j = 0; j < 2; j++) {
         var divName = "circularGraph" + i + "_" + j;
         var div = d3.select("body").append("div");
-        div.attr("id", divName);
+        div.attr("id", divName)
+           .attr("class", "smallgraph");
         circularGraphs[i][j] = new circularGraph("#" + divName, {diameter: 200});
     }
 }
